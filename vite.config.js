0ff21a18ca885a6d38dac531a9d8b83884244df8
JuanDevos <Juan.devos@ucp.edu.co>
2024-04-22 11:53:06 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3005, // Set your desired port number here
+    port: 3000, // Set your desired port number here
   },
 })
